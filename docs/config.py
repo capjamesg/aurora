c907939cf3dab65e0336674a91c47015a25d7cc9
capjamesg <jamesg@jamesg.blog>
2024-06-18 09:44:27 +0100

    add documentation

---
+++ docs/config.py
@@ -0,0 +1,14 @@
+import os
+
+BASE_URLS = {
+    "local": "http://localhost:8000",
+    "production": "https://aurora.jamesg.blog"
+}
+
+SITE_ENV = os.environ.get("SITE_ENV", "local")
+BASE_URL = BASE_URLS[SITE_ENV]
+ROOT_DIR = "pages"
+LAYOUTS_BASE_DIR = "_layouts"
+SITE_DIR = "_site"
+HOOKS = {}
+SITE_STATE = {}
